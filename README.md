8a03b5df592e176ac9cd88302c22c3d984514c4a
DmitryKravchuk <dmkr@qapint.com>
2015-07-28 11:42:13 +0300

    Fix android plugin for android cordova platform 4.0+

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A plugin to access the preference tags (settings) in config.xml via Javascript
 
 ## Usage
-    cordova plugin add com.wizky.cordova.appsettings
+    cordova plugin add com.kradmi.cordova.appsettings
 
     AppSettings.get(
     	function(value) {
@@ -11,6 +11,3 @@ A plugin to access the preference tags (settings) in config.xml via Javascript
     	function(error) {
         	alert("Error! " + JSON.stringify(error));
     }, ["Preference1", "Preference2"]);
-
-## Credits
-https://github.com/apache/cordova-labs/tree/cdvtest
--- src/android/AppSettings.java
+++ src/android/AppSettings.java
@@ -1,27 +1,25 @@
-package org.apache.cordova.appsettings;
-
-import java.util.Locale;
+package com.kradmi.cordova.appsettings;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaPreferences;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import java.util.Locale;
 
 public class AppSettings extends CordovaPlugin {
-
-
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     	JSONObject options = new JSONObject();
         if (action.equals("get")) {
         	try{
+                CordovaPreferences prefs = webView.getPreferences();
                 for(int i=0;i<args.length();i++){
-        	        String key=args.getString(i);
-                    String keyvalue = cordova.getActivity().getIntent().getStringExtra(key.toLowerCase(Locale.getDefault()));        		
-                    if (keyvalue != null) {
-                        options.put(key, keyvalue);
+        	        String key = args.getString(i).toLowerCase(Locale.getDefault());
+                    String value = prefs.getString(key, null);
+                    if (value != null) {
+                        options.put(key, value);
                     }
                 }
                 callbackContext.success(options);
